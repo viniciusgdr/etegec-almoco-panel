df297d9635f080f7178217f9fe6edc1b3a3a049a
Vinicius Silva <viniciusgdr@outlook.com>
2024-03-10 21:10:02 -0300

    feat: add next-PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/router';
 import { DashboardLayout } from '@viniciusgdr/components/layouts/Dashboard';
 import { Navbar } from '@viniciusgdr/components/Navbar';
 import { ThemeProvider, createTheme } from '@mui/material';
+import Head from 'next/head';
 
 const darkTheme = createTheme({
   palette: {
@@ -20,6 +21,24 @@ export default function App({ Component, pageProps, router: routerAppProps }: Ap
   const startPanelRoute = '/dashboard'
 
   return <SessionProvider session={pageProps.session}>
+    <Head>
+      <title>Almoço ETEGEC</title>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="etegec-almoco" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="theme-color" content="#000000" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="description" content="Horários de descida programados da ETEGEC" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href={'/etegec-almoco.png'} />
+      <meta property="og:image" content={'/etegec-almoco.png'} />
+      <meta property="og:title" content="Almoço ETEGEC" />
+      <meta property="og:description" content="Horários de descida programados da ETEGEC" />
+      <meta property="og:url" content="https://etegec-almoco.viniciusgdr.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Almoço ETEGEC" />
+    </Head>
     <ThemeProvider theme={darkTheme}>
       {
         router.asPath.startsWith(startPanelRoute) ? (
